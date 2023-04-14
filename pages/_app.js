2a866009dec913f88a60dd85607bcadd24b4ff91
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-14 11:00:48 +0200

    set colors and fonts for project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '../styles/globals.css'
+import Beaufort from 'next/font/local';
+import Spiegel from 'next/font/local';
+
+const beaufort = Beaufort({
+  src: '../public/fonts/BeaufortforLOL-Bold.otf',
+  variable: '--font-beaufort',
+});
+
+const spiegel = Spiegel({
+  src: '../public/fonts/Spiegel-Regular.otf',
+  variable: '--font-spiegel'
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${beaufort.variable} ${spiegel.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
