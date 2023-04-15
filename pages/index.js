d6ef70863e61401f9bea776c99a32802b327b7f3
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-15 14:44:23 +0200

    add gaps between components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Input from '../components/Input';
 import Button from '../components/Button';
+import classes from '../styles/index.module.css';
 export default function Home() {
   return (
     <>
       <h1>Welcome to LoreQuiz</h1>
+      <form className={classes.form}>
       <Input label='Enter your name to begin' input={{id: 'username'}} />
       <Button>Submit</Button>
+      </form>
     </>
   );
 }
