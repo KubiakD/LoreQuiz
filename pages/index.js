f756b8f182c9afcd1f5679fbcddcda79f1b2b68f
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-15 14:31:35 +0200

    add Button component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Input from '../components/Input'
+import Input from '../components/Input';
+import Button from '../components/Button';
 export default function Home() {
   return (
     <>
       <h1>Welcome to LoreQuiz</h1>
       <Input label='Enter your name to begin' input={{id: 'username'}} />
+      <Button>Submit</Button>
     </>
   );
 }
