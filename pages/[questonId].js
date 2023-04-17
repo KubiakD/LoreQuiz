148f7d2988e68ed1cda7ba5dbcb77c804e35ce27
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-17 15:47:41 +0200

    add question page

---
+++ pages/[questonId].js
@@ -0,0 +1,28 @@
+import Question from '../components/Question';
+import Answer from '../components/Answer';
+import Button from '../components/Button';
+import classes from '../styles/questionPage.module.css';
+export default function QuestionPage() {
+  const DUMMY_DATA = {
+    questionId: '515',
+    question: "What color are Teemo's eyes?",
+    answers: ['Green', 'Blue', 'Brown', "We don't know"],
+  };
+  return (
+    <>
+      <Question question={DUMMY_DATA.question} />
+      <form className={classes.answers}>
+        {DUMMY_DATA.answers.map((answer) => (
+          <Answer
+            answer={answer}
+            name={DUMMY_DATA.questionId}
+          />
+        ))}
+      </form>
+        <section className={classes.buttons}>
+        <Button>Next</Button>
+        <Button>Previous</Button>
+        </section>
+    </>
+  );
+}
