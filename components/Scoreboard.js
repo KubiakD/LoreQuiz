1316988443a2c222b22ac981eefe7d6abc8ddb36
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-18 13:55:06 +0200

    add tbody to table to avoid hydration fail

--- components/Scoreboard.js
+++ components/Scoreboard.js
@@ -2,17 +2,20 @@ import classes from './Scoreboard.module.css';
 const Scoreboard = (props) => {
   return (
     <table className={classes.scoreboard}>
-        
-      <th>Name</th>
-      <th>Score</th>
-      {props.ranking.map((user) => {
-        return (
-          <tr key={user.name}>
-            <td>{user.name}</td>
-            <td>{user.score}</td>
-          </tr>
-        );
-      })}
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Score</th>
+        </tr>
+        {props.ranking.map((user) => {
+          return (
+            <tr key={user.name}>
+              <td>{user.name}</td>
+              <td>{user.score}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
