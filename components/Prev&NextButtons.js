e3d37bc23fef0504d0abbc6f338b6cc29277c81f
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-24 11:29:01 +0200

    move next and previous buttons to separate component

---
+++ components/Prev&NextButtons.js
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { quizContext } from '../store/context';
+import Button from './Button';
+import classes from './Prev&NextButtons.module.css';
+const ButtonsSection = props => {
+    const router = useRouter();
+    const ctx = useContext(quizContext);
+
+    const nextQuestionHandler = () => {
+      ctx.userAnswers[props.currentQuestionIndex] = props.selectedAnswer;
+      ctx.setUserAnswers({ ...ctx.userAnswers });
+      if (props.currentQuestionIndex === 3) {
+        return router.push('/result');
+      }
+      props.setCurrentQuestionIndex(props.currentQuestionIndex + 1);
+      props.setAnswerIsSelected(false);
+    };
+
+    const previousQuestionHandler = () => {
+      props.setAnswerIsSelected(false);
+      props.setCurrentQuestionIndex(props.currentQuestionIndex - 1);
+    };
+ 
+
+return (
+  <section className={classes.buttons}>
+    <Button onClick={previousQuestionHandler}>Previous</Button>
+    <Button
+      onClick={nextQuestionHandler}
+      button={!props.answerIsSelected && { disabled: true }}
+    >
+      Next
+    </Button>
+  </section>
+);
+};
+
+export default ButtonsSection;
--- pages/questions.js
+++ pages/questions.js
@@ -1,40 +1,23 @@
 import Question from '../components/Question';
 import Answer from '../components/Answer';
-import Button from '../components/Button';
+import ButtonsSection from '../components/Prev&NextButtons';
 import classes from '../styles/questionPage.module.css';
 import { useContext, useState } from 'react';
 import { quizContext } from '../store/context';
-import { useRouter } from 'next/router';
 
 export default function QuestionPage() {
-const ctx = useContext(quizContext);
-const router = useRouter();
-const [selectedAnswer, setSelectedAnswer] = useState(null);
-const [answerIsSelected, setAnswerIsSelected] = useState(false);
-const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-const question = ctx.questions[0][currentQuestionIndex];
+  const ctx = useContext(quizContext);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [answerIsSelected, setAnswerIsSelected] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const question = ctx.questions[0][currentQuestionIndex];
 
-const changeHandler = event => {
-  const answer = event.target.value;
-  setSelectedAnswer(answer);
-  setAnswerIsSelected(true);
-};
-
-const nextQuestionHandler = () => {
-  ctx.userAnswers[currentQuestionIndex] = selectedAnswer;
-  ctx.setUserAnswers({...ctx.userAnswers});
-  if (currentQuestionIndex===3) {
-    return router.push('/result')
+  const changeHandler = (event) => {
+    const answer = event.target.value;
+    setSelectedAnswer(answer);
+    setAnswerIsSelected(true);
   };
- setCurrentQuestionIndex(currentQuestionIndex+1);
- setAnswerIsSelected(false);
-};
 
-const previousQuestionHandler = () => {
-  setAnswerIsSelected(false);
-  setCurrentQuestionIndex(currentQuestionIndex-1);
-
-};
   return (
     <>
       <Question question={question.question} />
@@ -44,15 +27,18 @@ const previousQuestionHandler = () => {
             answer={answer}
             name={question.questionId}
             key={answer}
-            checked = {selectedAnswer===answer}
+            checked={selectedAnswer === answer}
             onChange={changeHandler}
           />
         ))}
       </section>
-        <section className={classes.buttons}>
-        <Button onClick={previousQuestionHandler}>Previous</Button>
-        <Button onClick={nextQuestionHandler} button={!answerIsSelected&&{disabled: true}}>Next</Button>
-        </section>
+      <ButtonsSection
+        setCurrentQuestionIndex={setCurrentQuestionIndex}
+        currentQuestionIndex={currentQuestionIndex}
+        setAnswerIsSelected={setAnswerIsSelected}
+        answerIsSelected={answerIsSelected}
+        selectedAnswer={selectedAnswer}
+      />
     </>
   );
 }
