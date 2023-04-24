6407dcab5609ee6e256bd534d16a7e06ed25dcbc
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-24 11:32:20 +0200

    redirect from result page to index page

--- pages/result.js
+++ pages/result.js
@@ -3,6 +3,7 @@ import Scoreboard from '../components/Scoreboard';
 import Button from '../components/Button';
 import { useContext } from 'react';
 import { quizContext } from '../store/context';
+import { useRouter } from 'next/router';
 export default function ResultPage (props) {
   const DUMMY_DATA = {
     score: 5,
@@ -15,6 +16,10 @@ export default function ResultPage (props) {
   };
   const ctx = useContext(quizContext);
   const score = ctx.score;
+  const router = useRouter();
+  const clickHandler = () => {
+    return router.push('/');
+  };
   return (
     <>
       <h2>Your result</h2>
@@ -23,7 +28,7 @@ export default function ResultPage (props) {
       >{`${score}/${DUMMY_DATA.maxPoints}`}</span>
       <h2>Scoreboard</h2>
       <Scoreboard ranking={DUMMY_DATA.ranking} />
-      <Button>Play again</Button>
+      <Button onClick={clickHandler}>Play again</Button>
     </>
   );
 };
