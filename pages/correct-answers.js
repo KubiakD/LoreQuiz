936d63948562a0dff291cfe41dfc65b572a853e2
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-05-04 13:23:57 +0200

    create fallback for /correct-answers

--- pages/correct-answers.js
+++ pages/correct-answers.js
@@ -1,7 +1,13 @@
 import Head from 'next/head';
 import Carousel from '../components/Carousel';
 import { LinkBtn } from '../components/Button';
+import { useContext } from 'react';
+import { quizContext } from '../store/context';
 export default function correctAnswers() {
+  const ctx = useContext(quizContext);
+   if (!ctx.questions || ctx.questions.length === 0) {
+     return <h1>Something went wrong.</h1>;
+   }
     return (
       <>
         <Head>
