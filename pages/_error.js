a4dabd3a89b97d863f245e80e58fb1fd85ca754d
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-05-07 20:49:37 +0200

    modify _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,12 +1,20 @@
 import Link from 'next/link';
-export default function Error() {
+
+export default function Error({statusCode }) {
     return (
       <>
         <h1>Every mistake is a lesson</h1>
         <p>
-          Something went wrong. We will try to fix this as soon as possible.
+          An error {statusCode} occurred. While we fix it, you can read some
         </p>
+        <Link href='https://universe.leagueoflegends.com'>
+          League of Legends lore.
+        </Link>
       </>
     );
     
+};
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
 };
